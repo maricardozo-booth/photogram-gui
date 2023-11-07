4a48be8f6ec39aa47780de830dfd20aca44ac7fc
Mariana Cardozo <146393028+maricardozo-booth@users.noreply.github.com>
2023-11-07 23:18:31 +0000

    almost there

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -7,9 +7,36 @@ class PhotoController < ApplicationController
     the_id = params.fetch("path_id")
     matching_records = Photo.where ({ :id => the_id })
     @the_photo = matching_records.at(0)
-    
+
     render({ :template => "photo_templates/details"})
   end
 
-  
+  def insert
+    @new_photo = Photo.new
+    @new_photo.image = params.fetch("input_image")
+    @new_photo.caption = params.fetch("input_caption")
+    @new_photo.owner_id = params.fetch("input_owner_id")
+
+    if @new_photo.valid?
+      @new_photo.save
+      redirect_to("/photos", { :notice => "Photo created successfully." })
+    else
+      redirect_to("/photos", { :notice => "Photo failed to create successfully." })
+    end
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    @the_photo = Photo.where({ :id => the_id }).at(0)
+
+    @the_photo.image = params.fetch("input_image")
+    @the_photo.caption = params.fetch("input_caption")
+
+    if @the_photo.valid?
+      @the_photo.save
+      redirect_to("/photos/#{@the_photo.id}", { :notice => "Photo updated successfully."} )
+    else
+      redirect_to("/photos/#{@the_photo.id}", { :alert => "Photo failed to update successfully." })
+    end
+  end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -10,4 +10,30 @@ class UserController < ApplicationController
     render({ :template => "user_templates/details"})
   end
 
+  def insert
+    @new_user = User.new
+    @new_user.username = params.fetch("input_username")
+
+    if @new_user.valid?
+      @new_user.save
+      redirect_to("/users", { :notice => "User created successfully." })
+    else
+      redirect_to("/users", { :notice => "User failed to create successfully." })
+    end
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    @the_user = User.where({ :id => the_id }).at(0)
+
+    @the_user.username = params.fetch("input_username")
+
+    if @the_user.valid?
+      @the_user.save
+      redirect_to("/users/#{@the_user.id}", { :notice => "User updated successfully."} )
+    else
+      redirect_to("/users/#{@the_user.id}", { :alert => "User failed to update successfully." })
+    end
+  end
+
 end
